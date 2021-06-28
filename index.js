5dce981df1635b371a47f9a04ca8c58138b295f8
itsgabeover <gabe.enrique.miranda@gmail.com>
2021-06-28 11:19:11 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (ride > 2500) {
+    return 'No can do.'
+  }
+  else return 'I will gladly take your thirty bucks.';
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city == 'NYC'? (city = 'Ok, sounds good.') : (city = 'No go.');
+  return city;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      tip = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tip = 'Thank you.';
+      break;
+    default:
+      tip = 'Bye.';
+      break;
+  }
+  return tip;
+
+
 }
